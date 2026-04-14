78b458b7d246753a692a331276443207569fac7c
Sufyan Asghar <sufyanasghar231111@gmail.com>
2026-04-14 06:35:30 -0700

    Change in backend

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express=require('express')
 const app=express()
-
-const notes=[]
+let notes=[]
+app.use(express.json())
 
 app.post('/notes',(req,res)=>{
-    console.log(req.body);
+    notes.push(req.body)
+    res.status(201).json({
+        message:'post message successful'
+    })
     
 })
 
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        message:'get message Successful',
+        notes:notes
+    })
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    let index=req.params.index
+    delete notes[index]
+    res.status(200).json({
+        message:'delete Successful'
+    })
+
+})
 
 module.exports=app
